b3652432b40b7365071f995272b80fc035eecdca
Yoosuf Aathil <117569200+YoosufAathil@users.noreply.github.com>
2024-01-06 09:17:38 +0000

    added description, skills and expertise

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,17 +24,18 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
-
-/**
+  "I am currently pursuing a degree in Computing and Information Systems and am in my third year. I am passionate about acquiring practical experience as a software engineer and am actively seeking an internship opportunity to gain hands-on experience in the field."
+  
+  /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
+
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
+  "Web Development",
+  "Programming Languages",
+  "UI/UX design",
+  "Database Management",
   "Mobile user interfaces",
   "Graphic design",
 ];
@@ -45,7 +46,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my experience I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
